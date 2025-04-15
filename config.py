f0f1f0c863e2b1e8c57119e93cddf613fef804bc
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-04-15 21:34:05 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# Pyrogram setup
+API_ID = "28xx10"  # Replace this API ID with your actual API ID
+API_HASH = "7fc5b2xxb5eca3"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "800xxxxwowmU"  # Replace this BOT_TOKEN
