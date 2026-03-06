90614e0e33a9e51d4ee652404105de19be3213f8
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-03-06 11:10:37 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# Pyrogram setup
-API_ID = "28xx10"  # Replace this API ID with your actual API ID
-API_HASH = "7fc5b2xxb5eca3"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "800xxxxwowmU"  # Replace this BOT_TOKEN
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+COMMAND_PREFIX = ["#", ".", "/", "!", ","]
+ADMIN_ID = 7666341631
